2a0318009983a59af8602ce55076228000d3df01
ayush <ayush_bt18@iiitkalyani.ac.in>
2020-10-31 04:44:19 +0530

    second update

---
+++ Frontend/LocNavigator/app/src/main/java/com/example/locnavigator/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.locnavigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation anim;
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        ImageView imageView = findViewById(R.id.logo_id);
+        textView = findViewById(R.id.text);
+
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.top_wave); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(anim);
+    }
+
+}
